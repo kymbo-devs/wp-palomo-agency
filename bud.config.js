e6d6557dc3f90e45ac557b8f9917622f57491606
unknown <issabarake@gmail.com>
2024-06-14 20:23:14 -0500

    initialize project

--- bud.config.js
+++ bud.config.js
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://palomo-agency.local')
+    .setProxyUrl('https://skyblue-porcupine-132026.hostingersite.com')
     .watch(['resources', 'app']);
 
   /**
